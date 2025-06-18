9190a67630eca0c3e4c753c19f0b95fcc50ecdb9
antony martinez <antonymaruiz02@gmail.com>
2025-06-17 21:40:47 -0500

    Add: Basic class

---
+++ Basic/main.js
@@ -0,0 +1,51 @@
+console.info("Esto es una información");
+
+alert("HOLA, SOY UNA ALERTA");
+
+prompt("Dime tu nombre");
+
+/**
+ * Tipo de datos
+ * 1. Primitivos
+ * 2. Complejos
+ */
+
+// Primitivos
+
+/**
+ * number -> reales
+ * int -> entero (limite de espacio)
+ * float -> decimales
+ * string
+ * bool
+ * null
+ * undefined
+ * simbol
+ * bigInt
+ */
+
+// Complejos
+
+/**
+ * Arrays
+ * Objetos (Diccionario en python)
+ * Funciones
+ */
+
+
+/** Tipo de VARIABLES
+ * 1. let
+ * 2. const
+ * 3. var
+ */
+
+//  ------------------------------------
+
+let name = prompt("Name:");
+console.error(name);
+
+
+/**
+ * 1. Condicionales
+ * 2. Bucle for
+ */
